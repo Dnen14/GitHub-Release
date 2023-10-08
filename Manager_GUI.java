fa9049e038089f5372db99b2d044d50021a3e991
zakborman <zakborman@gmail.com>
2023-10-08 17:53:00 -0500

    Added more buttons for menu customization

--- Manager_GUI.java
+++ Manager_GUI.java
@@ -11,22 +11,22 @@ public class Manager_GUI extends JFrame {
     public static void main(String[] args)
     {
         //Building the connection
-        Connection conn = null;
-        try {
-        conn = DriverManager.getConnection(
-            "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_09m_db",
-            "csce315_909_NETID",
-            "PASSWORD");
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
-            System.exit(0);
-        }
-        JOptionPane.showMessageDialog(null,"Opened database successfully");
+        // Connection conn = null;
+        // try {
+        // conn = DriverManager.getConnection(
+        //     "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_09m_db",
+        //     "csce315_909_NETID",
+        //     "PASSWORD");
+        // } 
+        // catch (Exception e) {
+        //     e.printStackTrace();
+        //     System.err.println(e.getClass().getName()+": "+e.getMessage());
+        //     System.exit(0);
+        // }
+        // JOptionPane.showMessageDialog(null,"Opened database successfully");
 
         try {
-            // TODO: back end, specifics below
+            // TODO: back end (data retrieval), queries needed below
         } 
         catch (Exception e) {
             JOptionPane.showMessageDialog(null,"Error accessing Database.");
@@ -70,16 +70,25 @@ public class Manager_GUI extends JFrame {
         
         JPanel menuPanel = new JPanel();
 
+        JPanel menuButtonPanel = new JPanel();
+        menuButtonPanel.setLayout(new BoxLayout(menuButtonPanel, BoxLayout.Y_AXIS));
+
         // TODO: replace with name column of menu_item table
         String[] menuItems = {"Classic Milk Tea", "Taro Pearl Milk Tea"};
 
         JList<String> menuList = new JList<>(menuItems);
         JScrollPane menuPane = new JScrollPane(menuList);
-        menuPanel.add(menuPane);
         
         JTextField menuItemField = new JTextField(20);
         JTextField priceField = new JTextField(5);
-        JButton itemButton = new JButton("Update Menu");
+
+        JButton addItemButton = new JButton("Add");
+        JButton updateItemButton = new JButton("Update");
+        JButton deleteItemButton = new JButton("Delete");
+
+        addItemButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        updateItemButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        deleteItemButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         // TODO: replace with name column of ingredient table
         String[] ingredients = {"Tapioca Pearls", "Brown Sugar", "Straws"};
@@ -98,8 +107,8 @@ public class Manager_GUI extends JFrame {
             public void valueChanged(ListSelectionEvent e) {
                 if (!e.getValueIsAdjusting()) {
                     // TODO: Get data of item selected in list
-                    // fill menuItemField and priceField
-                    // select checkboxes of ingredients already used
+                    // fill menuItemField, priceField and select
+                    // checkboxes of ingredients in item
                 }
             }
         });
@@ -117,19 +126,37 @@ public class Manager_GUI extends JFrame {
             }
         });
 
-        itemButton.addActionListener(new ActionListener() {
+        addItemButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // TODO: implement menu changes
-                // Update if item already exists, else add new entry
+                // TODO: add item to menu_item
             }
         });
+
+        updateItemButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: update item of same name in menu_item
+            }
+        });
+
+        deleteItemButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: remove item from menu_item
+            }
+        });
+
+        menuPanel.add(menuPane);
+        menuButtonPanel.add(addItemButton);
+        menuButtonPanel.add(Box.createVerticalStrut(10)); 
+        menuButtonPanel.add(updateItemButton);
+        menuButtonPanel.add(Box.createVerticalStrut(10));
+        menuButtonPanel.add(deleteItemButton);
 
         menuPanel.add(new JLabel("Item Name: "));
         menuPanel.add(menuItemField);
         menuPanel.add(ingredientPane);
         menuPanel.add(new JLabel("Price: "));
         menuPanel.add(priceField);
-        menuPanel.add(itemButton);
+        menuPanel.add(menuButtonPanel);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
@@ -141,13 +168,13 @@ public class Manager_GUI extends JFrame {
         frame.setVisible(true);
 
         //closing the connection
-        try {
-            conn.close();
-            JOptionPane.showMessageDialog(null,"Connection Closed.");
-        } 
-        catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-        }
+        // try {
+        //     conn.close();
+        //     JOptionPane.showMessageDialog(null,"Connection Closed.");
+        // } 
+        // catch (Exception e) {
+        //     JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
+        // }
     }
 }
 

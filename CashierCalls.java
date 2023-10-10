d7fa0a2affbe6dd44cbbc44c27709d77bccf2a24
bat2492 <bat2492@tamu.edu>
2023-10-10 18:47:26 -0500

    implemented most of the Cashier Calls functions and fixed the MenuItem Class

--- CashierCalls.java
+++ CashierCalls.java
@@ -1,22 +1,104 @@
-import java.unil.ArrayList;
+import java.util.*;
+import java.sql.*;
 
 public final class CashierCalls extends SQLCalls{
-    public void submitOrder(long int order_id, ArrayList<MenuItem> items){
-
-    }
-
-    public long int getNextOrderId(){
-
-    }
-
-    public ArrayList<MenuItem> getMenuItems(){
-
-    }
-
-    public double getTotal(ArrayList<MenuItem> items){
-
-    }
-
     
+    
+    public static void submitOrder(ArrayList<MenuItem> items){
+
+    }
+
+    public static long getNextOrderId(){
+        Connection conn = null;
+        long id = -1;
+        try {
+        conn = DriverManager.getConnection(
+            "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_09m_db",
+            "csce315_909_bat2492",
+            "BT2415");
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        try{
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            
+            ResultSet rs = st.executeQuery("SELECT MAX(id) as max_id FROM order_table");
+            rs.next();
+            id = Long.valueOf(rs.getString("max_id")).longValue();
+        }
+        catch (Exception e){
+            System.out.println("DB Querry Failed");
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return id + 1;
+
+    }
+
+    public static ArrayList<MenuItem> getMenuItems(){
+        Connection conn = null;
+        try {
+        conn = DriverManager.getConnection(
+            "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_09m_db",
+            "csce315_909_bat2492",
+            "BT2415");
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        ArrayList<ArrayList<String>> items = new ArrayList<ArrayList<String>>();
+        try{
+            Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            SQLCalls s = new SQLCalls();
+
+            items = s.ViewTable(st,new String[]{"id","_size","price","name"},"menu_item");
+
+        }
+        catch (Exception e){
+            System.out.println("DB Querry Failed");
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        ArrayList<MenuItem> ret = new ArrayList<MenuItem>();
+        for(ArrayList<String> string_menu_item: items){
+            MenuItem item = new MenuItem(Long.valueOf(string_menu_item.get(0)).longValue(),//id
+                                         string_menu_item.get(3),//name
+                                         Double.valueOf(string_menu_item.get(2)).doubleValue());//price
+            String size = string_menu_item.get(1);
+            ItemSize enum_size = ItemSize.Medium;
+            if( size.toLowerCase().equals("small")){
+                enum_size = ItemSize.Small;
+            }
+            else if( size.toLowerCase().equals("large")){
+                enum_size = ItemSize.Large;
+            }
+            else{
+                enum_size = ItemSize.Medium;
+            }
+            item.setSize(enum_size);
+            ret.add(item);
+        }
+        
+        
+        return ret;
+    }
+
+    public static double getTotal(ArrayList<MenuItem> items){
+        double total = 0.0;
+        for(MenuItem item : items){
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public static void main(String[] args){
+        System.out.println("next Order Id is: \n" + getNextOrderId());
+        ArrayList<MenuItem> items = getMenuItems();
+        System.out.println("number of MenuItems: " + items.size());
+    }
+
 } 
 
